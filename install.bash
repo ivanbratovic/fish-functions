6247a855512af204958b979496b2a1a8da7d23a5
Ivan Bratović <ibratovic@neogov.net>
2024-03-11 10:16:13 +0100

    Use bash for install script since read '-p' is not POSIX

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 echo "Warning! This will overwrite all existing functions in ~/.config/fish/functions."
 read -p "Continue? [y/N] " answer
 if [ "$answer" ] && ([ "$answer" = "y" ] || [ "$answer" = "Y" ]); then
