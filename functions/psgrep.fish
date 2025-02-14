7ff625c5522e99ca370d32b4debd6d017c93f5e7
Ivan Bratović <ibratovic@neogov.net>
2025-02-14 13:14:21 +0100

    Update psgrep with minor fixes

--- functions/psgrep.fish
+++ functions/psgrep.fish
@@ -1,15 +1,15 @@
 function psgrep -d "Finds a process by a pattern in the output of the ps command."
     if test (count $argv) -lt 1; or test (count $argv) -gt 1
-        echo -e "psaux: Usage: \e[1mpsaux\e[0m \e[4mpattern\e[0m"
+        echo -e "Usage: \e[1mpsgrep\e[0m \e[4mpattern\e[0m"
         return -1
     end
-    set whole_outfile (mktemp || tempfile || echo "/tmp/tmp.psaux")
-    set filtered_outfile (mktemp || tempfile || echo "/tmp/tmp.psaux-filtered")
+    set whole_outfile (mktemp || tempfile || echo "/tmp/tmp.psgrep")
+    set filtered_outfile (mktemp || tempfile || echo "/tmp/tmp.psgrep-filtered")
     ps aux > $whole_outfile
     cat $whole_outfile | grep --color=always $argv | egrep -v "grep.*$argv" > $filtered_outfile
     if test (cat $filtered_outfile | wc -l) -gt 0
-       head -n 1 $filtered_outfile
-       cat $filtered_outfile
+       head -n 1 $whole_outfile # Title bar
+       /usr/bin/cat $filtered_outfile # Process list
     else
         echo "No processes found."
         return 1
