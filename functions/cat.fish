5780ff9e22ea4352861ca1a186033b4f270c7d04
Ivan Bratović <ivanbratovic4@gmail.com>
2025-08-04 09:50:52 +0200

    Add fallback to 'ls' for cat.fish

--- functions/cat.fish
+++ functions/cat.fish
@@ -1,3 +1,7 @@
-function cat --wraps='batcat' --description 'Use batcat if it is available'
+function cat --wraps='batcat' --description 'Use batcat if available, and ls if arg is dir'
     __fish_dynamic_alias cat "bat -P --theme=ansi" "batcat -P --theme=ansi" cat % $argv
+    if test $status -ne 0
+        # Try to `ls` if the argument is a directory
+        ls -l $argv 2>/dev/null
+    end
 end
